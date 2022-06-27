463282f04b6a2389cbf00fea9e11c31290815b0e
eikiyonemura <eikiyonemura@gmail.com>
2022-06-27 16:45:45 -0300

    Initial Commit

---
+++ Challenge Job-Readiness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge Job-Readiness
+//
+//  Created by Douglas Eiki Yonemura on 27/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
